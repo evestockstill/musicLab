de9613732fc9cb086d7ad94dc6852ecea2191995
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-02-07 14:48:01 -0800

    add loading to artists and fix fetch onload bug

--- src/components/artists/ArtistList.test.js
+++ src/components/artists/ArtistList.test.js
@@ -5,7 +5,7 @@ import ArtistList from './ArtistList';
 describe('ArtistList component', () => {
   it('matches a snapshot', () => {
     const wrapper = shallow(<ArtistList artists={[{ id: 1, name: 'test name' }, { id: 2, name: 'test name2' }]} match={{ params: { id: '1' }
-    }} />);
+    }} loading={true} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
